279c8f6c9565397e28878bcfa18c255aa64b3475
Juan Pablo <jpmontoya19@gmail.com>
2021-12-12 18:22:30 -0500

    Fix: Fixing raycast

--- Assets/Scr/Player/Player.cs
+++ Assets/Scr/Player/Player.cs
@@ -12,11 +12,13 @@ public class Player : MonoBehaviour
     private Vector2 _movementInput;
     private Quaternion _targetRotation;
     private Camera _cam;
+    private Plane _worldPlane;
 
     void Start()
     {
         _movementController = GetComponent<PlayerMovementController>();
         _cam = Camera.main;
+        _worldPlane = new Plane(Vector3.up, Vector3.zero);
     }
 
     // Update is called once per frame
@@ -42,12 +44,19 @@ public class Player : MonoBehaviour
     void CalculateTargetRotation()
     {
         Vector2 mouseScreenPosition = Input.mousePosition;
-        RaycastHit hit;
+        // RaycastHit hit;
         Ray ray = _cam.ScreenPointToRay(mouseScreenPosition);
 
-        if (Physics.Raycast(ray, out hit))
+        // if (Physics.Raycast(ray, out hit))
+        // {
+        //     Vector3 dir = (hit.point - transform.position).normalized;
+        //     _targetRotation = Quaternion.LookRotation(dir);
+        // }
+
+        if (_worldPlane.Raycast(ray, out float distanceToPlane))
         {
-            Vector3 dir = (hit.point - transform.position).normalized;
+           Vector3 pointHit = ray.GetPoint(distanceToPlane);
+           Vector3 dir = (pointHit - transform.position).normalized;
             _targetRotation = Quaternion.LookRotation(dir);
         }
     }
