09eb116bcc0740c826b87c65a1aa27d7935f45ba
Juan Pablo <jpmontoya19@gmail.com>
2021-12-12 14:54:23 -0500

    Add: Asteroid movements

--- Assets/Samples/Asteroids/OrbitGameObject.cs
+++ Assets/Samples/Asteroids/OrbitGameObject.cs
@@ -6,16 +6,26 @@ public class OrbitGameObject : MonoBehaviour
 {
     [SerializeField] private float m_RadiusMin = 5;
     [SerializeField] private float m_RadiusMax = 10;
+    [SerializeField] private float m_SpeedMin = 0.5f;
+    [SerializeField] private float m_SpeedMax = 1.0f;
     [SerializeField] private int m_Count = 10;
     [SerializeField] private Mesh m_Mesh;
     [SerializeField] private Material m_Material;
 
-    private List<GameObject> m_Asteroids;
+    private List<AsteroidDescription> m_Asteroids;
+    private float m_Time;
+    private struct AsteroidDescription
+    {
+        public GameObject AsteroidGO;
+        public float Radius;
+        public float Angle0;
+        public float Speed;
+    }
 
     void Start()
     {
         // Instance all objects
-        m_Asteroids = new List<GameObject>();
+        m_Asteroids = new List<AsteroidDescription>();
 
         for (int i = 0; i < m_Count; i++)
         {
@@ -30,12 +40,31 @@ public class OrbitGameObject : MonoBehaviour
             var meshRenderer = asteroid.AddComponent<MeshRenderer>();
             meshRenderer.material = m_Material;
 
-            m_Asteroids.Add(asteroid);
+            var asteroidDescription = new AsteroidDescription
+            {
+                AsteroidGO = asteroid,
+                Radius = Random.Range(m_RadiusMin, m_RadiusMax),
+                Angle0 = Random.Range(0, 2 * Mathf.PI),
+                Speed = Random.Range(m_SpeedMin, m_SpeedMin),
+            };
+
+            m_Asteroids.Add(asteroidDescription);
         }
     }
 
     void Update()
     {
-        
+        // Move objects
+        m_Time += Time.deltaTime;
+        for (int i = 0; i < m_Asteroids.Count; i++)
+        {
+            var asteroidDescription = m_Asteroids[i];
+            var angle = asteroidDescription.Speed * m_Time + asteroidDescription.Angle0;
+            var position = new Vector3();
+            position.x = asteroidDescription.Radius * Mathf.Cos(angle);
+            position.y = 0;
+            position.z = asteroidDescription.Radius * Mathf.Sin(angle);
+            asteroidDescription.AsteroidGO.transform.position = position;
+        }
     }
 }
