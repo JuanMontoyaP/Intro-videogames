2db1aff28fa7b8a4a5b76d0ae5be93dd18789a22
Juan Pablo <jpmontoya19@gmail.com>
2021-12-12 21:51:07 -0500

    Add: Gun and bullets

--- Assets/Scr/Player/Player.cs
+++ Assets/Scr/Player/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private PlayerMovementController _movementController;
+    private GunController _gunController;
 
     [SerializeField] private float speed = 6.5f;
     [SerializeField] private float _rotationSpeed = 30f;
@@ -13,10 +14,12 @@ public class Player : MonoBehaviour
     private Quaternion _targetRotation;
     private Camera _cam;
     private Plane _worldPlane;
+    private bool _isShooting;
 
     void Start()
     {
         _movementController = GetComponent<PlayerMovementController>();
+        _gunController = GetComponent<GunController>();
         _cam = Camera.main;
         _worldPlane = new Plane(Vector3.up, Vector3.zero);
     }
@@ -32,6 +35,15 @@ public class Player : MonoBehaviour
 
         // _targetRotation = Quaternion.LookRotation(targetMovementDirection);
         _movementController.RotateTo(_targetRotation, _rotationSpeed);
+
+        if (_isShooting)
+        {
+            _gunController.OnTriggerHold();
+        }
+        else
+        {
+
+        }
     }
 
     void ProcessInput()
@@ -39,6 +51,8 @@ public class Player : MonoBehaviour
         _movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
         CalculateTargetRotation();
+
+        _isShooting = Input.GetButton("Fire1");
     }
 
     void CalculateTargetRotation()
---
+++ Assets/Scr/Weapon/Projectile.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float _speed = 10;
+    [SerializeField] private float _lifeTime = 3;
+
+    private float _timeToDisable = 3;
+
+    void Start()
+    {
+        _timeToDisable = _lifeTime;
+    }
+
+    void Update()
+    {
+        if (_timeToDisable <= 0)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        _timeToDisable -= Time.deltaTime;
+
+        float movementDistance = _speed * Time.deltaTime;
+        Vector3 translation = Vector3.forward * movementDistance;
+        transform.Translate(translation);
+    }
+}
